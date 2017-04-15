a07e6e5f84d4ffb0a9a403142cac80f6ba103dcf
Darius Costolas <dariuscostolas@yahoo.com>
2017-04-15 20:24:40 +0300

    Implemented google analytics

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+declare const ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
 }
